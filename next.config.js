9e451f84c6e3276c802fde0c34d460a7c5691d7c
shaunsaker <shaun@aux.co.za>
2019-01-12 16:35:25 +0200

    Add next-fonts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withImages = require('next-images');
 const withSASS = require('@zeit/next-sass');
+const withFonts = require('next-fonts');
 const compose = require('next-compose');
 
+const fontsConfig = {};
 const imagesConfig = {};
 const sassConfig = {};
 
 module.exports = compose([
+  [withFonts, fontsConfig],
   [withImages, imagesConfig],
   [withSASS, sassConfig],
   {
