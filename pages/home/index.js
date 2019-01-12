55455835c1695a89b86625fd015a5ec9120a4633
shaunsaker <shaun@aux.co.za>
2019-01-12 12:08:01 +0200

    Remove Home page content

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'next/link';
 
 import Page from '../../components/Page';
 import Header from '../../components/Header';
@@ -12,20 +11,7 @@ const Home = () => {
       <Header />
 
       <div className="flex">
-        <h1>
-          Welcome to Astrid Bradley Photography | Wedding &amp; LIfestyle Photographer based in Cape
-          Town, South Africa
-        </h1>
-
-        <p>Browse through my galleries to see a selection of my work.</p>
-
-        <p>
-          Please feel free to contact me with any comments or questions{' '}
-          <Link href="/contact">
-            <span className="link">here</span>
-          </Link>
-          .
-        </p>
+        <div />
       </div>
 
       <Footer />
