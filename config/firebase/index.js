05c170e1f082cd835e9d715950bbd90a2161a31a
shaunsaker <shaun@aux.co.za>
2019-01-21 17:49:51 +0200

    Add firebase config

---
+++ config/firebase/index.js
@@ -0,0 +1,10 @@
+const firebase = {
+  apiKey: 'AIzaSyDQ7mxBfpZvrFnVvIbwqy-UGNg1Faw-akI',
+  authDomain: 'astrid-bradley-photography.firebaseapp.com',
+  databaseURL: 'https://astrid-bradley-photography.firebaseio.com',
+  projectId: 'astrid-bradley-photography',
+  storageBucket: 'astrid-bradley-photography.appspot.com',
+  messagingSenderId: '176676489001',
+};
+
+export default firebase;
---
+++ config/index.js
@@ -0,0 +1,9 @@
+import firebase from './firebase';
+
+const config = {
+  firebase,
+};
+
+export { firebase };
+
+export default config;
