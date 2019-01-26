be9136d7e9e7729961707575b0d150ad0095ce6f
shaunsaker <shaun@aux.co.za>
2019-01-26 10:32:02 +0200

    Add blank Admin page

---
+++ pages/admin/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Page from '../../components/Page';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const Admin = () => {
+  return (
+    <Page>
+      <Header />
+
+      <main>
+        <div />
+      </main>
+
+      <Footer />
+    </Page>
+  );
+};
+
+Admin.getInitialProps = async () => {};
+
+Admin.propTypes = {};
+Admin.defaultProps = {};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Admin);
