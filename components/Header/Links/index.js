5ae321e067fda7e868b5847a554d2d645b544d50
shaunsaker <shaun@aux.co.za>
2019-01-27 11:05:24 +0200

    Add admin routes to Header component

--- components/Header/Links/index.js
+++ components/Header/Links/index.js
@@ -1,40 +1,71 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
 import Link from 'next/link';
+import { connect } from 'react-redux';
 
 import ROUTES from '../routes';
 
-const Links = ({ router }) => {
+const Links = ({ router, isAdmin }) => {
   const { pathname, query } = router;
   const { id } = query;
 
-  return ROUTES.map((route) => {
-    const { href, title, prefetch, as } = route;
-    let isActive = href === pathname;
+  return (
+    <Fragment>
+      {ROUTES.map((route) => {
+        const { href, title, prefetch, as, admin } = route;
+        let isActive = href === pathname;
 
-    // IF the actual route is the category page
-    // IF the mapped route is the category page
-    // IF the mapped routes id matches that of the actual route
-    if (
-      pathname.indexOf('category') > -1 &&
-      href.indexOf('category') > -1 &&
-      href.indexOf(id) > -1
-    ) {
-      isActive = true;
-    }
+        // IF the actual route is the category page
+        // IF the mapped route is the category page
+        // IF the mapped routes id matches that of the actual route
+        if (
+          pathname.indexOf('category') > -1 &&
+          href.indexOf('category') > -1 &&
+          href.indexOf(id) > -1
+        ) {
+          isActive = true;
+        }
 
-    return (
-      <li key={href}>
-        <Link href={href} prefetch={prefetch} as={as}>
-          <span className={`nav-link ${isActive && 'active'}`}>{title}</span>
-        </Link>
-      </li>
-    );
-  });
+        // IF the mapped route is an admin route
+        // IF the user is admin
+        // OR if the mapped route is not an admin route
+        // THEN return it
+        if ((admin && isAdmin) || !admin) {
+          return (
+            <li key={href}>
+              <Link href={href} prefetch={prefetch} as={as}>
+                <span className={`nav-link ${isActive && 'active'}`}>{title}</span>
+              </Link>
+            </li>
+          );
+        }
+
+        return null;
+      })}
+    </Fragment>
+  );
 };
 
-Links.propTypes = {};
+Links.propTypes = {
+  router: PropTypes.shape({
+    pathname: PropTypes.string,
+    query: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+  isAdmin: PropTypes.bool,
+};
 Links.defaultProps = {};
 
-export default withRouter(Links);
+function mapStateToProps(state) {
+  const { user } = state;
+  const { uid } = user;
+  const isAdmin = uid ? true : false;
+
+  return {
+    isAdmin,
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(Links));
--- components/Header/routes.js
+++ components/Header/routes.js
@@ -3,34 +3,46 @@ const routes = [
     title: 'Home',
     href: '/',
     prefetch: false,
+    admin: false,
   },
   {
     title: 'Weddings',
     href: '/category?id=weddings',
     as: '/category/weddings',
     prefetch: false,
+    admin: false,
   },
   {
     title: 'Lifestyle',
     href: '/category?id=lifestyle',
     as: '/category/lifestyle',
     prefetch: false,
+    admin: false,
   },
   {
     title: 'Styled Shoots',
     href: '/category?id=styled-shoots',
     as: '/category/styled-shoots',
     prefetch: false,
+    admin: false,
   },
   {
     title: 'Photo Queue',
     href: '/photo-queue',
     prefetch: false,
+    admin: false,
   },
   {
     title: 'Contact',
     href: '/contact',
     prefetch: true,
+    admin: false,
+  },
+  {
+    title: 'Admin',
+    href: '/admin',
+    prefetch: false,
+    admin: true,
   },
 ];
 
