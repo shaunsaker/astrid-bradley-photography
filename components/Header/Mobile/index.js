eab521e25ef7eeb05360523dd4b9894a12636ee5
shaunsaker <shaun@aux.co.za>
2019-01-31 10:54:21 +0200

    Give Header shadow on mobile scroll

--- components/Header/Mobile/index.js
+++ components/Header/Mobile/index.js
@@ -12,16 +12,43 @@ export class Mobile extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleScroll = this.handleScroll.bind(this);
+    this.setHasShadow = this.setHasShadow.bind(this);
     this.toggleMenu = this.toggleMenu.bind(this);
 
     this.state = {
       showMenu: false,
+      hasShadow: false,
     };
   }
 
   static propTypes = {};
 
   static defaultProps = {};
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const { scrollY } = window;
+
+    if (scrollY !== 0) {
+      this.setHasShadow(true);
+    } else {
+      this.setHasShadow(false);
+    }
+  }
+
+  setHasShadow(hasShadow) {
+    this.setState({
+      hasShadow,
+    });
+  }
 
   toggleMenu() {
     const { showMenu } = this.state;
@@ -32,11 +59,11 @@ export class Mobile extends React.Component {
   }
 
   render() {
-    const { showMenu } = this.state;
+    const { showMenu, hasShadow } = this.state;
     const menuComponent = showMenu && <Menu handleClose={this.toggleMenu} />;
 
     return (
-      <div className="container hidden-md-up">
+      <div className={`container hidden-md-up ${hasShadow && 'shadow-lg'}`}>
         <div className="buttonContainer">
           <button type="button" onClick={this.toggleMenu}>
             <MenuIcon size={24} color={colors.black} />
