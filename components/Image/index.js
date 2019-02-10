f3b1bce181ce273ffea9bdf837ffafd91a24a6e2
shaunsaker <shaun@aux.co.za>
2019-02-10 15:06:42 +0200

    Fix Image rendering

--- components/Image/index.js
+++ components/Image/index.js
@@ -3,26 +3,75 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const Image = ({ src, alt, width, height, id, children }) => {
-  return (
-    <div className="container relative">
-      <img src={src} alt={alt} id={id} width={width} height={height} className="image" />
+import Spinner from '../Spinner';
 
-      {children}
+export default class Image extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <style jsx>{styles}</style>
-    </div>
-  );
-};
+    this.onLoad = this.onLoad.bind(this);
+    this.onError = this.onError.bind(this);
+    this.setIsLoading = this.setIsLoading.bind(this);
 
-Image.propTypes = {
-  src: PropTypes.string,
-  alt: PropTypes.string,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  id: PropTypes.string,
-  children: PropTypes.node,
-};
-Image.defaultProps = {};
+    this.state = {
+      isLoading: true,
+    };
+  }
 
-export default Image;
+  static propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    id: PropTypes.string,
+    children: PropTypes.node,
+  };
+
+  static defaultProps = {};
+
+  onLoad() {
+    this.setIsLoading(false);
+  }
+
+  onError() {
+    this.setIsLoading(false);
+  }
+
+  setIsLoading(isLoading) {
+    this.setState({
+      isLoading,
+    });
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    const { src, alt, width, height, id, children } = this.props;
+    const loadingComponent = isLoading && (
+      <div className="loading-container abs-stretch flex-center">
+        <Spinner small />
+      </div>
+    );
+
+    return (
+      <div className="container relative">
+        <img
+          src={src}
+          alt={alt}
+          id={id}
+          width={width}
+          height={height}
+          className="image"
+          style={{ width, height }}
+          onLoad={this.onLoad}
+          onError={this.onError}
+        />
+
+        {children}
+
+        {loadingComponent}
+
+        <style jsx>{styles}</style>
+      </div>
+    );
+  }
+}
--- components/Image/styles.js
+++ components/Image/styles.js
@@ -7,6 +7,7 @@ const styles = css`
   }
 
   .image {
+    object-fit: cover;
   }
 
   .loading-container {
