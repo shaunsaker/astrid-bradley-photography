b0200ee7a3b91ac69ad43d9f10877639a1f26429
shaunsaker <shaun@aux.co.za>
2019-02-13 16:30:06 +0200

    Add productForm to config

--- config/forms/index.js
+++ config/forms/index.js
@@ -1,11 +1,13 @@
 import packageForm from './packageForm';
+import productForm from './productForm';
 import shootForm from './shootForm';
 
 const forms = {
   packageForm,
+  productForm,
   shootForm,
 };
 
-export { packageForm, shootForm };
+export { packageForm, productForm, shootForm };
 
 export default forms;
---
+++ config/forms/productForm/index.js
@@ -0,0 +1,16 @@
+const productForm = [
+  {
+    type: 'text',
+    name: 'name',
+    label: 'Name',
+    isRequired: true,
+  },
+  {
+    type: 'number',
+    name: 'price',
+    label: 'Price (R)',
+    isRequired: true,
+  },
+];
+
+export default productForm;
