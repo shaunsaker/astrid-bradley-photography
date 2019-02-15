58f4cfee2b4272a440a92a7fec05c0e683614564
shaunsaker <shaun@aux.co.za>
2019-02-15 07:47:15 +0200

    Append products to EditPackage form

--- layouts/EditPackage/index.js
+++ layouts/EditPackage/index.js
@@ -18,21 +18,59 @@ export class EditPackage extends React.Component {
 
   static propTypes = {
     packages: PropTypes.arrayOf(PropTypes.shape({})),
+    products: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        id: PropTypes.string,
+      }),
+    ),
     packageID: PropTypes.string,
   };
 
   static defaultProps = {};
 
   render() {
-    const { packages, packageID } = this.props;
+    const { packages, packageID, products } = this.props;
     const packageDocument = packages.filter((item) => item.id === packageID)[0];
-    const { name } = packageDocument;
-    const title = `Editing: ${name}`;
+    const title = `Editing: ${packageDocument.name}`;
+    const extraFields = [
+      {
+        type: 'group',
+        name: 'products-included',
+        label: 'Products included:',
+        fields: products.map((product) => {
+          const { id, name } = product;
+          const label = `${name} (qty)`;
+
+          return {
+            type: 'number',
+            name: id,
+            label,
+          };
+        }),
+      },
+      {
+        type: 'group',
+        name: 'products-available',
+        label: 'Products available as add-ons:',
+        fields: products.map((product) => {
+          const { id, name } = product;
+
+          return {
+            type: 'checkbox',
+            name: id,
+            label: name,
+          };
+        }),
+      },
+    ];
+
+    const formWithExtraFields = packageForm.concat(extraFields);
 
     return (
       <Layout title={title}>
         <EditDocumentSection
-          form={packageForm}
+          form={formWithExtraFields}
           document={packageDocument}
           collectionURL="packages"
         />
@@ -42,8 +80,11 @@ export class EditPackage extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+  const { packages, products } = state;
+
   return {
-    packages: state.packages,
+    packages,
+    products,
   };
 };
 
