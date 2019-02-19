8e108d6aa67a3f38831b188a60dbb33431f301e0
shaunsaker <shaun@aux.co.za>
2019-02-19 15:39:46 +0200

    Remove unused PackageItem styles

--- components/PackageItem/styles.js
+++ components/PackageItem/styles.js
@@ -1,13 +1,5 @@
 import css from 'styled-jsx/css'; // eslint-disable-line
 
-import { colors, rhythm } from '../../static/styles/styleConstants';
-
-const styles = css`
-  .container {
-    padding: ${rhythm.vt}px ${rhythm.hz}px;
-    background-color: ${colors.white};
-    border: 1px solid ${colors.lightGrey};
-  }
-`;
+const styles = css``;
 
 export default styles;
