939f36937db9a00a423b31771ea34986bca521be
shaunsaker <shaun@aux.co.za>
2019-03-16 13:08:55 +0200

    Add firestore/getCollection service

--- src/app/services/firestore/index.js
+++ src/app/services/firestore/index.js
@@ -1,15 +1,17 @@
 import batchUpdate from './batchUpdate';
+import getCollection from './getCollection';
 import getRef from './getRef';
 import setDocument from './setDocument';
 import sync from './sync';
 
 const firestore = {
   batchUpdate,
+  getCollection,
   getRef,
   setDocument,
   sync,
 };
 
-export { batchUpdate, getRef, setDocument, sync };
+export { batchUpdate, getCollection, getRef, setDocument, sync };
 
 export default firestore;
