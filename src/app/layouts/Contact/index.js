68c9090c0b9031544280cd876be39aa3b181ea6f
shaunsaker <shaun@aux.co.za>
2019-03-17 10:52:03 +0200

    Add success state to Contact form submission

--- src/app/layouts/Contact/index.js
+++ src/app/layouts/Contact/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { createUID } from '../../utils';
+import { business } from '../../config';
 import FIELDS from './fields';
 
 import Layout from '../../components/Layout';
 import Form from '../../components/Form';
 import QuoteSection from '../../components/QuoteSection';
+import HeadingText from '../../components/HeadingText';
+import ParagraphText from '../../components/ParagraphText';
 
 import withSaveDocument from '../../enhancers/withSaveDocument';
 
@@ -15,13 +19,24 @@ export class Contact extends React.Component {
     super(props);
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.setSubmitSuccess = this.setSubmitSuccess.bind(this);
 
     this.state = {};
   }
 
   static propTypes = {
     onSaveDocument: PropTypes.func,
+    isSaving: PropTypes.bool,
   };
+
+  componentDidUpdate(prevProps) {
+    const { isSaving } = this.props;
+
+    if (!isSaving && prevProps.isSaving) {
+      // If we saved successfully
+      this.setSubmitSuccess(true);
+    }
+  }
 
   onSubmit(values) {
     const { onSaveDocument } = this.props;
@@ -31,12 +46,30 @@ export class Contact extends React.Component {
     onSaveDocument(values, url);
   }
 
+  setSubmitSuccess(submitSuccess) {
+    this.setState({
+      submitSuccess,
+    });
+  }
+
   render() {
+    const { submitSuccess } = this.state;
+    const formComponent = submitSuccess ? (
+      // FIXME: New component
+      <Fragment>
+        <HeadingText>Form submitted successfully.</HeadingText>
+
+        <div className="spacer-vt" />
+
+        <ParagraphText>{`${business.name}  will be in touch shortly.`}</ParagraphText>
+      </Fragment>
+    ) : (
+      <Form formName="contact" fields={FIELDS} submitText="Send" handleSubmit={this.onSubmit} />
+    );
+
     return (
       <Layout title="Get in Touch">
-        <section>
-          <Form formName="contact" fields={FIELDS} submitText="Send" handleSubmit={this.onSubmit} />
-        </section>
+        <section>{formComponent}</section>
 
         <QuoteSection />
       </Layout>
@@ -44,4 +77,14 @@ export class Contact extends React.Component {
   }
 }
 
-export default withSaveDocument(Contact);
+const mapStateToProps = (state) => {
+  const { appState } = state;
+  const { pendingTransactions } = appState;
+  const isSaving = pendingTransactions.length ? true : false;
+
+  return {
+    isSaving,
+  };
+};
+
+export default withSaveDocument(connect(mapStateToProps)(Contact));
