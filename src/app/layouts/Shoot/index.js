3ed11d346d6cc18c5bd0ccfc7fb5d77b06f7fa59
shaunsaker <shaun@aux.co.za>
2019-04-02 15:32:44 +0200

    Get the shoot in getInitialProps

--- src/app/layouts/Shoot/index.js
+++ src/app/layouts/Shoot/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'next/router';
 
 import Layout from '../../components/Layout';
 import ShootSection from './ShootSection';
-import LoadingSection from '../../components/LoadingSection';
 import ContactButton from '../../components/ContactButton';
 
-const Shoot = ({ router, shoots }) => {
-  const shootID = router.query.id;
-  const shoot = shoots.filter((item) => item.id === shootID)[0];
-  let title;
-  let description;
-  let shootComponent = <LoadingSection />;
-
-  if (shoot) {
-    title = shoot.name;
-    description = `${shoot.name} on the ${shoot.date} at ${shoot.location}`;
-    shootComponent = <ShootSection shoot={shoot} />;
-  }
+const Shoot = ({ shoot }) => {
+  const title = shoot.name;
+  const description = `${shoot.name} on the ${shoot.date} at ${shoot.location}`;
+  const shootComponent = <ShootSection shoot={shoot} />;
 
   return (
     <Layout title={title} description={description}>
@@ -31,18 +20,13 @@ const Shoot = ({ router, shoots }) => {
 };
 
 Shoot.propTypes = {
-  // connect
-  shoots: PropTypes.arrayOf(PropTypes.shape({})),
-
-  // withRouter
-  router: PropTypes.shape({ query: PropTypes.shape({ id: PropTypes.string }) }),
+  // getInitialProps
+  shoot: PropTypes.shape({
+    name: PropTypes.string,
+    date: PropTypes.number,
+    location: PropTypes.string,
+  }),
 };
 Shoot.defaultProps = {};
 
-const mapStateToProps = (state) => {
-  return {
-    shoots: state.shoots,
-  };
-};
-
-export default withRouter(connect(mapStateToProps)(Shoot));
+export default Shoot;
--- src/app/pages/shoot/index.js
+++ src/app/pages/shoot/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import { signInAnonymously } from '../../services/auth';
+import { getCollection } from '../../services/firestore';
+
 import Shoot from '../../layouts/Shoot';
 
 const Page = (props) => {
   return <Shoot {...props} />;
 };
 
-Page.getInitialProps = async () => {};
+Page.getInitialProps = async ({ query }) => {
+  const { id } = query;
+
+  await signInAnonymously();
+
+  const { collection } = await getCollection({ url: 'shoots', query: ['id', '==', id] });
+  const shoot = collection[0];
+
+  return { shoot };
+};
 
 export default Page;
